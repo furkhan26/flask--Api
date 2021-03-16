7389c244442b7e141a5473bfe9e3b4320fdca8c5
furkhan26 <furkhanmehdi26@gmail.com>
2021-03-16 22:07:27 +0530

    first

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
+from datetime import datetime
 from flask_restful import Api, Resource, reqparse, inputs, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from datetime import datetime
 
 
 app = Flask(__name__)
