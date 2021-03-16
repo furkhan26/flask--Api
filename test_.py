f05e419ebcf5255012c8cf4d2b3073eb85952f0f
Syed Furkhan Mehdi <53236773+furkhan26@users.noreply.github.com>
2021-03-16 22:13:50 +0530

    Update test_.py

--- test_.py
+++ test_.py
@@ -1,9 +1,1 @@
-import requests
 
-BASE = "http://127.0.0.1:5000/"
-
-response = requests.put(BASE + "ProcessPayment/",{"CreditCardNumber":1234,"CardHolder":"michale","ExpirationDate":"2020-01-20","Amount": 23444})
-print(response.json())
-
-# response = requests.get(BASE + "ProcessPayment/4")
-# print(response.json())
